dc2ffe6c326a65704429de0061eb27240787079b
Alexander Hofmeister <alexander.hofmeister@ebalance.ch>
2023-04-16 09:45:13 +0200

    Try adding content layer manually

---
+++ .contentlayer/.cache/v0.3.1/compiled-contentlayer-config-6N6RULPO.mjs
@@ -0,0 +1,50 @@
+// contentlayer.config.js
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import remarkGfm from "remark-gfm";
+import rehypePrettyCode from "rehype-pretty-code";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      description: "The title of the post",
+      required: true
+    },
+    publishedAt: {
+      type: "date",
+      description: "The date of the post",
+      required: true
+    }
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/blog/${post._raw.flattenedPath}`
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "src/posts",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: "one-dark-pro",
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted");
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-6N6RULPO.mjs.map
