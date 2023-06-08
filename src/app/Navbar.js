5670e7477d648e0378a6b7a781a7e25fe05209df
Alexander Hofmeister <alexander.hofmeister@ebalance.ch>
2023-06-08 05:57:42 +0200

    Add highlighting for current route

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,20 +1,24 @@
+"use client"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 
-const NavbarLink = ({ href, label }) =>
+const NavbarLink = ({ href, label, currentPath }) =>
   <Link href={href}
-        className="hover:text-pastel-green-300 mx-5">
+        className={`hover:text-pastel-green-300 mx-5  ${currentPath === href ? 'border-b border-b-pastel-green-300': ''}`}>
     {label}
   </Link>
 
-const Navbar = function() {
+const Navbar = () => {
+  const currentPath = usePathname();
+
   return <nav className="sticky top-0 border-b border-b-gray">
     <div className="flex items-center mx-auto p-4">
       <Link href="/" className={"mr-20"}>
         <span className="text-xl">Alexander Hofmeister</span>
       </Link>
-      <NavbarLink href={'/about'} label={'About'}/>
-      <NavbarLink href={'/blog'} label={'Blog'}/>
-      <NavbarLink href={'/projects'} label={'Projects'}/>
+      <NavbarLink href={'/about'} label={'About'} currentPath={currentPath}/>
+      <NavbarLink href={'/blog'} label={'Blog'} currentPath={currentPath}/>
+      <NavbarLink href={'/projects'} label={'Projects'} currentPath={currentPath}/>
   </div>
 </nav>
 }
