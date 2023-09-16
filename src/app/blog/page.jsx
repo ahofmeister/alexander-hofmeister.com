e770adef63c0632cf02a40bc883f740dca4acf65
Alexander Hofmeister <alexander.hofmeister@ebalance.ch>
2023-09-16 15:46:07 +0200

    Try notion integration

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,25 +2,25 @@ import { compareDesc, format, parseISO } from 'date-fns'
 import { allPosts } from 'contentlayer/generated'
 import Link from 'next/link'
 
-async function getData() {
+async function getPosts() {
   return allPosts.sort((a, b) => {
     return compareDesc(new Date(a.publishedAt), new Date(b.publishedAt))
   })
 }
 
-export default async function Home() {
+export default async function Blog() {
 
-  const posts = await getData()
+  const posts = await getPosts()
 
   return (
     <div className="mx-auto max-w-2xl py-16 text-center">
       {posts.map((post, id) => (
-        <PostCard key={id} {...post} />
+        <Post key={id} {...post} />
       ))}
     </div>)
 }
 
-const PostCard = post => (
+const Post = post => (
   <div className="mb-6">
     <time dateTime={post.publishedAt} className="block text-sm text-slate-600">
       {format(parseISO(post.publishedAt), 'LLLL d, yyyy')}
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,3 +1,25 @@
-export default async function Projects() {
-  return <>A list of personal projects coming soon</>
+const { Client } = require('@notionhq/client')
+
+export const revalidate = 3600 * 2
+
+async function getProjects() {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY })
+
+  const response = await notion.databases.query({ database_id: process.env.NOTION_PROJECTS_DB })
+  return response.results
 }
+
+export default async function Projects() {
+
+  const projects = await getProjects()
+
+  return (
+    <div className="mx-auto max-w-2xl py-16 text-center">
+      {projects.map(project => {
+        // eslint-disable-next-line react/jsx-key
+        return <div>{project.properties.title.title[0].plain_text}</div>
+      })}
+
+    </div>)
+}
+
