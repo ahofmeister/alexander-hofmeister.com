3e52f9a18cf9754df2beb512adcc68cdc7398eac
Alexander Hofmeister <alexander.hofmeister@ebalance.ch>
2023-09-21 17:52:26 +0200

    Highlight current page even in sub pages

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,23 +1,23 @@
-"use client"
+'use client'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation'
 
 const NavbarLink = ({ href, label, currentPath }) =>
   <Link href={href}
-        className={`hover:text-pastel-green-300 mx-5  ${currentPath === href ? 'border-b border-b-pastel-green-300': ''}`}>
+        className={`hover:text-pastel-green-300 mx-5  ${currentPath.startsWith(href) ? 'border-b border-b-pastel-green-300' : ''}`}>
     {label}
   </Link>
 
 const Navbar = () => {
-  const currentPath = usePathname();
+  const currentPath = usePathname()
 
   return <nav className="sticky top-0 border-b border-b-gray">
     <div className="flex justify-center p-4">
       <NavbarLink href={'/about'} label={'About'} currentPath={currentPath}/>
       <NavbarLink href={'/blog'} label={'Blog'} currentPath={currentPath}/>
       <NavbarLink href={'/projects'} label={'Projects'} currentPath={currentPath}/>
-  </div>
-</nav>
+    </div>
+  </nav>
 }
 
 export default Navbar
