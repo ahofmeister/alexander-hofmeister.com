90c6f27c5386bd353f7c030a9aa4aa8c2d784250
Alexander Hofmeister <alexander.hofmeister@ebalance.ch>
2023-09-21 08:11:39 +0200

    Improve navbar

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -12,10 +12,7 @@ const Navbar = () => {
   const currentPath = usePathname();
 
   return <nav className="sticky top-0 border-b border-b-gray">
-    <div className="flex items-center mx-auto p-4">
-      <Link href="/" className={"mr-20"}>
-        <span className="text-xl">Alexander Hofmeister</span>
-      </Link>
+    <div className="flex justify-center p-4">
       <NavbarLink href={'/about'} label={'About'} currentPath={currentPath}/>
       <NavbarLink href={'/blog'} label={'Blog'} currentPath={currentPath}/>
       <NavbarLink href={'/projects'} label={'Projects'} currentPath={currentPath}/>
